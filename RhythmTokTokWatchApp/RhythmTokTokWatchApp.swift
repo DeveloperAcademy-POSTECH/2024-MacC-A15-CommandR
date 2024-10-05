ef46e996e576a27a9a8ec8dae1fcfd175dd3f017
Rokdam (Wendy) <30626388+rokdam@users.noreply.github.com>
2024-10-05 17:05:31 +0900

    Setting: XCode 프로젝트 생성 (#8)
    * Setting: XCode 프로젝트 생성
    - SwiftLint 적용
    - Appearance-Light 적용
    - Watch 앱 Target 추가
    
    * Rename: WatchApp 디렉토리명 변경
    * Rename: WatchApp 디렉토리명 띄어쓰기 변경

---
+++ RhythmTokTokWatchApp/RhythmTokTokWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  RhythmTokTokWatchApp.swift
+//  RhythmTokTokWatchApp
+//
+//  Created by 백록담 on 10/5/24.
+//
+
+import SwiftUI
+
+@main
+struct RhythmTokTokWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
