d8e27ce348118a3bbc2c961894b0e25e328552fe
Kkugy <tjdrnrdla@hanmail.net>
2024-10-06 19:22:44 +0900

    Feat: mediaMannager에 musicXmlParser 연결

--- RhythmTokTok/MediaMannager.swift
+++ RhythmTokTok/MediaMannager.swift
@@ -10,4 +10,25 @@ import AVFoundation
 
 struct MediaMannager {
     
+    func getMediaFile(xmlData: Data) {
+        Task {
+            let parsedMeasures = await parseMusicXMLData(xmlData: xmlData)
+        }
+    }
+    
+    func parseMusicXMLData(xmlData: Data) async -> [Measure]{
+        
+        let parser = MusicXMLParser()
+        let measures = await parser.parseMusicXML(from: xmlData)
+        
+        // 파싱이 완료된 후에 작업을 처리, 확인용 프린트
+//        print("Parsed measures count: \(measures.count)")
+//        for measure in measures {
+//            for note in measure.notes {
+//                print("Pitch: \(note.pitch), Duration: \(note.duration)")
+//            }
+//        }
+//        
+        return measures
+    }
 }
