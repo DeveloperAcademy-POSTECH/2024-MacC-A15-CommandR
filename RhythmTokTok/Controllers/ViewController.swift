99563bd2a326d3b90dd36d2df6b7807b7c2a2346
Kkugy <tjdrnrdla@hanmail.net>
2024-10-06 20:22:51 +0900

    LoadingViewController 연결

--- RhythmTokTok/Controllers/ViewController.swift
+++ RhythmTokTok/Controllers/ViewController.swift
@@ -8,10 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let button = UIButton(type: .system)
+        button.setTitle("로딩뷰", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(navigateToNewViewController), for: .touchUpInside)
+        
+        view.addSubview(button)
+        
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+    @objc private func navigateToNewViewController() {
+        
+        let loadingViewController = LoadingViewController()
+        
+         present(loadingViewController, animated: true, completion: nil)
+
     }
 }
 
