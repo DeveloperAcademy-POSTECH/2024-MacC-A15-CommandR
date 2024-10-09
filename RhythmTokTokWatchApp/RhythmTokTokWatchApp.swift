04ed547e273f0e39a7ed0b953483d067f8d49df6
Bell-isHere <167271769+Bell-isHere@users.noreply.github.com>
2024-10-09 16:41:39 +0900

    워치커넥티비티로 아이폰에서 워치 앱 켜짐 감지하기 (#18)
    * feat: iOS에서 워치앱 활성화 여부 확인  (audioKit 패키지 제거함)
    
    * Chore: watchOS 폴더 생성
    
    * faet: iOS앱 매니저, Watch앱 매니저 분리. 오류메세지 양식 적용. 주석 정리.
    
    * Chore: Viewcontroller 코드 간결화

--- RhythmTokTokWatchApp/RhythmTokTokWatchApp.swift
+++ RhythmTokTokWatchApp/RhythmTokTokWatchApp.swift
@@ -4,14 +4,18 @@
 //
 //  Created by 백록담 on 10/5/24.
 //
+// RhythmTokTokWatchApp.swift
 
 import SwiftUI
 
 @main
 struct RhythmTokTokWatchApp: App {
+    @StateObject private var connectivityManager = ConnectivityManager()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(connectivityManager)
         }
     }
 }
