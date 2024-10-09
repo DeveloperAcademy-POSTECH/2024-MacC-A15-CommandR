fb5df9f8ae44f5277127c70761dbfe58a180daf4
Bell-isHere <167271769+Bell-isHere@users.noreply.github.com>
2024-10-09 16:41:39 +0900

    워치커넥티비티로 아이폰에서 워치 앱 켜짐 감지하기 (#18)
    * feat: iOS에서 워치앱 활성화 여부 확인  (audioKit 패키지 제거함)
    
    * Chore: watchOS 폴더 생성
    
    * faet: iOS앱 매니저, Watch앱 매니저 분리. 오류메세지 양식 적용. 주석 정리.
    
    * Chore: Viewcontroller 코드 간결화

--- RhythmTokTok/Temp/Bell/WatchManager.swift
+++ RhythmTokTok/Temp/Bell/WatchManager.swift
@@ -3,5 +3,66 @@
 //  RhythmTokTok
 //
 //  Created by sungkug_apple_developer_ac on 10/8/24.
-//
 
+
+import WatchConnectivity
+import UIKit
+
+// 워치 연결 상태 변경 알림을 위한 Notification 이름 정의
+extension Notification.Name {
+    static let watchConnectivityStatusChanged = Notification.Name("watchConnectivityStatusChanged")
+}
+
+class WatchManager: NSObject, WCSessionDelegate {
+    
+    static let shared = WatchManager()
+    
+    private override init() {
+        super.init()
+        setupSession()
+    }
+    
+    private func setupSession() {
+        guard WCSession.isSupported() else {
+            print("Error [WatchManager]: WCSession 지원되지 않음")
+            return
+        }
+        
+        let session = WCSession.default
+        session.delegate = self
+        session.activate()
+        print("iOS 앱에서 WCSession 활성화 요청")
+    }
+    
+    // MARK: - WCSessionDelegate 메서드
+    
+    // 세션 활성화 완료 시 호출
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        if activationState == .activated {
+            print("iOS 앱에서 WCSession 활성화 완료")
+        }
+        if let error = error {
+            print("Error [WatchManager]: WCSession 활성화 실패 - \(error.localizedDescription)")
+        }
+        
+        // 상태 변경 알림 전송
+        NotificationCenter.default.post(name: .watchConnectivityStatusChanged, object: nil)
+    }
+    
+    // 워치 앱의 연결 상태가 변경될 때 호출
+    func sessionReachabilityDidChange(_ session: WCSession) {
+        print("워치 앱 연결 상태 변경됨: \(session.isReachable)")
+        
+        // 상태 변경 알림 전송
+        NotificationCenter.default.post(name: .watchConnectivityStatusChanged, object: nil)
+    }
+    
+    func sessionDidBecomeInactive(_ session: WCSession) {
+        print("WCSession 비활성화됨")
+    }
+    
+    func sessionDidDeactivate(_ session: WCSession) {
+        WCSession.default.activate()
+        print("WCSession 다시 활성화 요청")
+    }
+}
--- RhythmTokTok/ViewControllers/ViewController.swift
+++ RhythmTokTok/ViewControllers/ViewController.swift
@@ -4,34 +4,74 @@
 //
 //  Created by 백록담 on 10/5/24.
 //
+// ViewController.swift
+
 
 import UIKit
+import WatchConnectivity
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    let statusLabel = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupUI()
+        
+        let _ = WatchManager.shared
+        updateWatchAppStatus()
+        setupObservers()
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self, name: .watchConnectivityStatusChanged, object: nil)
+    }
+    
+    @objc private func navigateToNewViewController() {
+        let loadingViewController = LoadingViewController()
+        present(loadingViewController, animated: true, completion: nil)
+    }
+    
+    // 워치 앱 상태 업데이트 메서드
+    @objc func updateWatchAppStatus() {
+        DispatchQueue.main.async {
+            let session = WCSession.default
+            if session.isReachable {
+                self.statusLabel.text = "워치 앱 켜짐"
+                self.statusLabel.textColor = UIColor.systemGreen
+            } else {
+                self.statusLabel.text = "워치 앱 꺼짐"
+                self.statusLabel.textColor = UIColor.systemRed
+            }
+        }
+    }
+}
+
+extension ViewController {
+    func setupUI() {
         let button = UIButton(type: .system)
         button.setTitle("로딩뷰", for: .normal)
         button.translatesAutoresizingMaskIntoConstraints = false
         button.addTarget(self, action: #selector(navigateToNewViewController), for: .touchUpInside)
-        
         view.addSubview(button)
+        
+        statusLabel.translatesAutoresizingMaskIntoConstraints = false
+        statusLabel.textAlignment = .center
+        view.addSubview(statusLabel)
         
         NSLayoutConstraint.activate([
             button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            button.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -30),
+            
+            statusLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            statusLabel.topAnchor.constraint(equalTo: button.bottomAnchor, constant: 20)
         ])
     }
     
-    @objc private func navigateToNewViewController() {
-        
-        let loadingViewController = LoadingViewController()
-        
-         present(loadingViewController, animated: true, completion: nil)
-
+    // NotificationCenter 관찰자 설정을 별도의 메서드로 분리
+    func setupObservers() {
+        NotificationCenter.default.addObserver(self, selector: #selector(updateWatchAppStatus), name: .watchConnectivityStatusChanged, object: nil)
     }
 }
-
-
