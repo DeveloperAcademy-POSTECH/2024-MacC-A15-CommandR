7285dbf7ee39d9aa20cb5c4e22440ede51b66a04
Kkugy <tjdrnrdla@hanmail.net>
2024-10-11 16:02:55 +0900

    Chore: 리베이스 데브

--- RhythmTokTok/ViewControllers/LoadingViewController.swift
+++ RhythmTokTok/ViewControllers/LoadingViewController.swift
@@ -15,8 +15,6 @@ class LoadingViewController: UIViewController {
     private var wavFilePathURL: URL?
     private var midiFilePathURL: URL?
     private var cancellables = Set<AnyCancellable>()
-    private var midiPlayer: AVMIDIPlayer? // MIDI 재생 플레이어
-    private var lastPosition: TimeInterval = 0
     private var isPlayingMIDIFile = false
     private var currentBPM = 120 // bpm 조절 설정
 
@@ -78,8 +76,9 @@ class LoadingViewController: UIViewController {
     private func setupActions() {
         playMusicXMLButton.isEnabled = false
         playMusicXMLButton.addTarget(self, action: #selector(playMusicXML), for: .touchUpInside)
-        
-        playMIDIFileButton.addTarget(self, action: #selector(playMIDIFIle), for: .touchUpInside)
+
+        playMIDIFileButton.isEnabled = false
+        playMIDIFileButton.addTarget(self, action: #selector(playMIDIFile), for: .touchUpInside)
         
         // combine으로 계속 값 업데이트
         musicPlayer.$currentTime.sink { [weak self] time in
@@ -89,12 +88,12 @@ class LoadingViewController: UIViewController {
     
     @objc private func playMusicXML() {
         if isPlayingMusicXML {
-            musicPlayer.pause()
+            musicPlayer.wavFilePause()
         } else {
             guard let outputPathURL = midiFilePathURL else { return }
             if FileManager.default.fileExists(atPath: outputPathURL.path) {
                 musicPlayer.loadAudioFile(url: outputPathURL)
-                musicPlayer.play()
+                musicPlayer.wavFilePlay()
             } else {
                 ErrorHandler.handleError(errorMessage: "Audio file not found at path \(outputPathURL.path)")
             }
@@ -102,13 +101,13 @@ class LoadingViewController: UIViewController {
         isPlayingMusicXML.toggle()
     }
     
-    @objc private func playMIDIFIle() {
+    @objc private func playMIDIFile() {
         if isPlayingMIDIFile {
-            pauseMIDI()
+            musicPlayer.resumeMIDI()
         } else {
-            guard let outputPathURL = wavFilePathURL else { return }
+            guard let outputPathURL = midiFilePathURL else { return }
             if FileManager.default.fileExists(atPath: outputPathURL.path) {
-                playMIDIFile(midiURL: midiFilePathURL!)
+                musicPlayer.playMIDI()
             } else {
                 print("Error [LoadingViewController]: Audio file not found at path \(outputPathURL.path)")
             }
@@ -117,8 +116,8 @@ class LoadingViewController: UIViewController {
     }
     
     private func generateMusicXMLAudio() {
-        // MusicXML file 로드
-        guard let xmlPath = Bundle.main.url(forResource: "moon", withExtension: "xml") else {
+        // MusicXML 파일 로드
+        guard let xmlPath = Bundle.main.url(forResource: "MahlFaGe4Sample", withExtension: "musicxml") else {
             print("Error [LoadingViewController]: Failed to find MusicXML file in bundle.")
             return
         }
@@ -127,57 +126,31 @@ class LoadingViewController: UIViewController {
             do {
                 let xmlData = try Data(contentsOf: xmlPath)
                 print("Successfully loaded MusicXML data.")
-                // 미디어 파일 만들기
+                
+                // MediaManager 인스턴스 생성
                 let mediaManager = MediaManager()
+                
+                // WAV 파일 및 MIDI 파일을 각각 동기적으로 생성
                 wavFilePathURL = try await mediaManager.getMediaFile(xmlData: xmlData)
+                if let wavFilePathURL = wavFilePathURL {
+                    playMusicXMLButton.isEnabled = true
+                    print("WAV file created successfully: \(wavFilePathURL)")
+                } else {
+                    print("Error: wav file URL is nil.")
+                }
+
                 midiFilePathURL = try await mediaManager.getMIDIFile(xmlData: xmlData)
-                print("Completed. Media file path: \(wavFilePathURL?.path ?? "No file created")")
-                playMusicXMLButton.isEnabled = true
+                if let midiFilePathURL = midiFilePathURL {
+                    musicPlayer.setMIDIFile(midiURL: midiFilePathURL)
+                    playMIDIFileButton.isEnabled = true
+                    print("MIDI file created successfully: \(midiFilePathURL)")
+                } else {
+                    print("Error: MIDI file URL is nil.")
+                }
+
             } catch {
                 ErrorHandler.handleError(error: error)
             }
         }
-    }
-    
-    private func playMIDIFile(midiURL: URL) {
-        do {
-            let bankURL = Bundle.main.url(forResource: "Piano", withExtension: "sf2")! // 사운드 폰트 파일 경로
-            
-            midiPlayer = try AVMIDIPlayer(contentsOf: midiURL, soundBankURL: bankURL)
-            midiPlayer?.prepareToPlay()
-            
-            // BPM 조정
-            midiPlayer?.rate = Float(2.5)
-            
-            // 이전에 일시 정지된 위치에서 재개
-            midiPlayer?.currentPosition = lastPosition
-            midiPlayer?.play()
-            
-            print("MIDI file is playing.")
-        } catch {
-            print("Error [LoadingViewController]: Failed to play MIDI file: \(error.localizedDescription)")
-        }
-    }
-
-    // 일시 정지
-    private func pauseMIDI() {
-        guard let midiPlayer = midiPlayer else { return }
-        
-        // 현재 재생 시간을 저장
-        lastPosition = midiPlayer.currentPosition
-        midiPlayer.stop()
-        
-        print("MIDI playback paused at \(lastPosition) seconds.")
-    }
-
-    // 재개
-    private func resumeMIDI() {
-        guard let midiPlayer = midiPlayer else { return }
-        
-        // 저장된 위치에서 다시 재생
-        midiPlayer.currentPosition = lastPosition
-        midiPlayer.play()
-        
-        print("MIDI playback resumed from \(lastPosition) seconds.")
     }
 }
