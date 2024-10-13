85297ae9f22dd4737e9fae4887857654efe26949
Kkugy <tjdrnrdla@hanmail.net>
2024-10-13 13:24:22 +0900

    Feat: 모든 옥타브 MIDI 노트 번호 추출 구현

--- RhythmTokTok/Models/Note.swift
+++ RhythmTokTok/Models/Note.swift
@@ -18,12 +18,19 @@ struct Note {
     
     // pitch 값을 MIDI 노트 번호로 변환하는 함수
     func pitchNoteNumber() -> Int {
-        let pitchMap: [String: Int] = [
-            "C4": 60, "C#4": 61, "D4": 62, "D#4": 63, "E4": 64,
-            "F4": 65, "F#4": 66, "G4": 67, "G#4": 68, "A4": 69,
-            "A#4": 70, "B4": 71, "C5": 72
+        let pitchBase: [String: Int] = [
+            "C": 0, "C#": 1, "D": 2, "D#": 3, "E": 4,
+            "F": 5, "F#": 6, "G": 7, "G#": 8, "A": 9,
+            "A#": 10, "B": 11
         ]
         
-        return pitchMap[pitch] ?? 60 // 디폴트 값은 C4 (MIDI 노트 60)
+        // MIDI 노트 번호는 C4 = 60을 기준으로, 옥타브마다 12씩 차이남
+        let baseNoteNumber = 60 // C4의 MIDI 번호
+        guard let pitchOffset = pitchBase[pitch] else {
+            return baseNoteNumber // pitch가 없으면 C4로 기본값 반환
+        }
+        
+        // 옥타브에 따른 노트 번호 계산
+        return baseNoteNumber + (octave - 4) * 12 + pitchOffset
     }
 }
