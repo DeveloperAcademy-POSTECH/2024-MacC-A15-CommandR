271cdd40f07e0102ee0f3875576274d373ef961b
Kkugy <tjdrnrdla@hanmail.net>
2024-10-13 17:33:42 +0900

    Feat: watch info.plist 백그라운드 workout-process 추가

---
+++ RhythmTokTokWatchApp/View/PlayHapticView.swift
@@ -0,0 +1,18 @@
+//
+//  PlayHapticView.swift
+//  RhythmTokTokWatchApp
+//
+//  Created by sungkug_apple_developer_ac on 10/13/24.
+//
+
+import SwiftUI
+
+struct PlayHapticView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlayHapticView()
+}
