3bbd0a4a7894b06856bc8b722f7d911fccd40561
leesungkug <91677242+leesungkug@users.noreply.github.com>
2024-10-14 21:32:48 +0900

    햅틱 시퀀스 워치에서 백그라운드 동작 가능하게 구현 (#37)
    * Feat: watch info.plist 백그라운드 workout-process 추가
    
    * Feat: watch햅틱뷰안에 타이머 인터벌 추가
    
    * Feat: 햅틱스케줄메니저 추가
    
    * Feat: 함수 쪼개기, 설정 함수 추가
    
    * Feat: 워치 탭뷰로 PlayHipticView 연결
    
    * Feat: PlayHapticView 수정
    
    * Remove: 불필요한 info.plist 항목 제거, 팀 제거

---
+++ RhythmTokTokWatchApp/View/PlayHapticView.swift
@@ -0,0 +1,80 @@
+//
+//  PlayHapticView.swift
+//  RhythmTokTokWatchApp
+//
+//  Created by sungkug_apple_developer_ac on 10/13/24.
+//
+
+import SwiftUI
+
+struct PlayHapticView: View {
+    @State private var HapticManager = HapticScheduleManager()
+    @State private var tempo: Double = 120.0  // 템포 초기값 (BPM)
+    @State private var isPlay = false
+
+    // 나중에 여기에 실행할 햅틱 대입해주면 됨
+    let exampleBeatTimes: [Double] = [0.5/*쉼표 1.0 추가*/, 0.5, 1.0, 0.5, 0.5,
+                               1.0, 1.0, 3.0/*쉼표 1.0 추가*/,
+                               0.5, 0.5, 1.0, 0.5, 0.5,
+                               1.0, 1.0, 2.0/*쉽표 1.0추가*/,
+                               1.5/*쉼표 1.0 추가*/, 0.5, 1.0, 0.5, 0.5,
+                               1.0, 1.0, 1.0, 1.0,
+                               2.0, 2.0,
+                               4.0,
+                               //도돌이표A
+                               0.5/*쉼표 1.0 추가*/, 0.5, 1.0, 0.5, 0.5,
+                               1.0, 1.0, 3.0/*쉼표 1.0 추가*/,
+                               0.5, 0.5, 1.0, 0.5, 0.5,
+                               1.0, 1.0, 2.0/*쉽표 1.0추가*/,
+                               1.5/*쉼표 1.0 추가*/, 0.5, 1.0, 0.5, 0.5,
+                               1.0, 1.0, 1.0, 1.0,
+                               2.0, 2.0,
+                               4.0,
+                               //도돌이표A
+                               1.0, 1.0, 1.0, 1.0,
+                               1.0, 0.5, 0.5, 0.5, 0.5, 1.0,
+                               1.0, 1.0, 1.0, 1.0,
+                               1.0, 0.5, 0.5, 0.5, 0.5, 1.0,
+                               1.0, 1.0, 1.0, 1.0,
+                               1.0, 0.5, 0.5, 1.0, 1.0,
+                               5.0/*쉼표 2.0 추가 + 쉼표 1.0 추가*/,
+                               1.0, 0.5, 0.5, 1.0,
+                               // 도돌이표 B
+                               0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 1.0,
+                               0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 1.0,
+                               0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5,
+                               0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 1.0,
+                               // 도돌이표 B
+                               0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 1.0,
+                               0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 1.0,
+                               0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5,
+                               0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 2.0/*쉼표 1.0 추가*/,
+                               // 도돌이표 B
+                               1.0, 1.0, 1.0]
+
+    var body: some View {
+        VStack {
+            Text("Pick Me")
+                .font(.title)
+                .padding()
+            
+            Button(action: {
+                if isPlay {
+                    HapticManager.stopHaptic()
+                } else {
+                    HapticManager.starHaptic(beatTime: exampleBeatTimes)
+                }
+                
+                isPlay.toggle()
+            }) {
+                Text(isPlay ? "정지" : "햅틱 시작")
+                    .font(.headline)
+            }
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    PlayHapticView()
+}
