07e8820bd25c4c6366715ddee6d95dd5e161c5c8
Bell-isHere <167271769+Bell-isHere@users.noreply.github.com>
2024-10-14 16:49:15 +0900

    유저디폴트 설정, 설정뷰 초안 작업, 워치커넥티비티 코드 복구 작업 (#30)
    * Feat: 설정뷰 초안 그리기. 유저디폴트 설정.
    
    * feat: 설정뷰 수정. 슬라이더 안움직이는 이슈있음.
    
    * feat: 설정뷰 글자크기 슬라이더 제거 후 버튼으로 변경
    
    * Fix: 워치 앱 감지여부 코드 수정
    
    * feat: 테스트 코드 원복
    
    * Chore: enum 파일 분리
    
    * fear: setupActions 반복문 설정으로 코드 수정
    
    * Chore: 코드 리뷰 반영하여 코드 구조화
    
    * Chore: UserDefaults 파일명 변경
    
    * Chore: 버튼 배열로 수정
    
    * Chore: 버튼을 배열로 변경
    
    * Chore: 코드 리뷰 반영

---
+++ RhythmTokTok/Temp/Bell/Notification.swift
@@ -0,0 +1,13 @@
+//
+//  Notification.swift
+//  RhythmTokTok
+//
+//  Created by Byeol Kim on 10/9/24.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let watchConnectivityStatusChanged = Notification.Name("watchConnectivityStatusChanged")
+    static let fontSizeChanged = Notification.Name("fontSizeChanged")
+}
