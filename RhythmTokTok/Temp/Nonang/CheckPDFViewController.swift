feb022c6a7e1ddebc021d4c7bc9c96deabf30791
norang00 <norang1221@gmail.com>
2024-10-14 23:46:02 +0900

    [PR반영]CheckPDFViewController 의 가독성 향상

--- RhythmTokTok/Temp/Nonang/CheckPDFViewController.swift
+++ RhythmTokTok/Temp/Nonang/CheckPDFViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Kyuhee Hong on 10/13/24.
 //
-// PDF 파일을 보여주는 확인용 뷰 컨트롤러
+
 import UIKit
 import PDFKit
 
@@ -12,30 +12,36 @@ class PDFConfirmationViewController: UIViewController {
     
     var fileURL: URL?
     var pdfView: PDFView!
+    var confirmButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
 
-        // PDFView 생성 및 설정
+        setupUI()
+        setupConstraints()
+        setupActions()
+        
+        loadPDFDocument()
+    }
+    
+    // UI 구성
+    private func setupUI() {
+        // PDFView 설정
         pdfView = PDFView()
         pdfView.translatesAutoresizingMaskIntoConstraints = false
         pdfView.autoScales = true
         view.addSubview(pdfView)
         
-        if let fileURL = fileURL {
-            let pdfDocument = PDFDocument(url: fileURL)
-            pdfView.document = pdfDocument
-        }
-
-        // 확인 버튼 추가
-        let confirmButton = UIButton(type: .system)
+        // 확인 버튼 설정
+        confirmButton = UIButton(type: .system)
         confirmButton.setTitle("이 파일이 맞습니다", for: .normal)
-        confirmButton.addTarget(self, action: #selector(confirmButtonTapped), for: .touchUpInside)
         confirmButton.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(confirmButton)
-        
-        // 제약조건 설정
+    }
+    
+    // 제약조건 설정
+    private func setupConstraints() {
         NSLayoutConstraint.activate([
             pdfView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             pdfView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
@@ -48,9 +54,22 @@ class PDFConfirmationViewController: UIViewController {
             confirmButton.heightAnchor.constraint(equalToConstant: 50)
         ])
     }
+    
+    // 버튼 액션 설정
+    private func setupActions() {
+        confirmButton.addTarget(self, action: #selector(confirmButtonTapped), for: .touchUpInside)
+    }
 
-    // 확인 버튼을 눌렀을 때 동작
-    @objc func confirmButtonTapped() {
+    // PDF 파일 로드
+    private func loadPDFDocument() {
+        if let fileURL = fileURL {
+            let pdfDocument = PDFDocument(url: fileURL)
+            pdfView.document = pdfDocument
+        }
+    }
+
+    // 확인 버튼 액션
+    @objc private func confirmButtonTapped() {
         print("사용자가 PDF 파일을 확인하고 선택을 완료했습니다.")
         // 모달로 띄운 경우 dismiss 사용
         self.dismiss(animated: true, completion: nil)
