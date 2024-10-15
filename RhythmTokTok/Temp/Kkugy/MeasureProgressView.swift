4b6b87fc1f158d793f27c0bc1803c13a80e6ca46
leesungkug <91677242+leesungkug@users.noreply.github.com>
2024-10-15 22:40:05 +0900

    Music practice view 구현 (#58)
    * Feat: MusicPracticeView 생성 및 제목 스택 구현
    
    * Feat: divider 추가
    
    * Feat: viewController 네비게이션으로 연결
    
    * Feat: 뷰제약 설정
    
    * Feat: 줄 프로그래스 테이블 추가
    
    * Feat: 프로그래스안에 타이틀 추가 및 연결 로직 구현
    
    * Feat: 재생 일시정지 버튼 추가
    
    * Feat: resumeButton 추가
    
    * Remove: code 정리

---
+++ RhythmTokTok/Temp/Kkugy/MeasureProgressView.swift
@@ -0,0 +1,106 @@
+//
+//  MeasureProgressView.swift
+//  RhythmTokTok
+//
+//  Created by sungkug_apple_developer_ac on 10/15/24.
+//
+
+import UIKit
+
+class MeasureProgressView: UIView {
+    private let progressView = UIView()
+    private let progressIndicatorImageView = UIImageView()
+    private var progressWidthConstraint: NSLayoutConstraint!
+    
+    let titleHeader: UILabel = {
+        let label = UILabel()
+        label.text = "🎼"
+        label.font = UIFont.systemFont(ofSize: 32, weight: .bold)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = ""
+        label.font = UIFont.systemFont(ofSize: 18, weight: .medium)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    var progress: CGFloat = 0 {
+        didSet {
+            updateProgress()
+        }
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+
+    private func setupView() {
+        // 기본 레이아웃 설정
+        self.backgroundColor = .gray03
+        self.layer.cornerRadius = 12
+        self.clipsToBounds = true
+
+        // ProgressView 설정
+        progressView.backgroundColor = .progress
+        progressView.layer.cornerRadius = 0
+        progressView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(progressView)
+        
+        // 이미지 뷰 설정
+        progressIndicatorImageView.image = UIImage(named: "progressCursor")
+        progressIndicatorImageView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(progressIndicatorImageView)
+        addSubview(titleHeader)
+        addSubview(titleLabel)
+
+        // 레이아웃 제약 조건
+        progressWidthConstraint = progressView.widthAnchor.constraint(equalToConstant: 0)
+        NSLayoutConstraint.activate([
+            progressView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            progressView.topAnchor.constraint(equalTo: topAnchor),
+            progressView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            progressWidthConstraint,
+            
+            // 이미지 뷰 제약 조건
+            progressIndicatorImageView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            progressIndicatorImageView.heightAnchor.constraint(equalToConstant: 62),
+            
+            // 타이틀 제약 조건
+            titleHeader.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 12),
+            titleHeader.centerYAnchor.constraint(equalTo: centerYAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: titleHeader.trailingAnchor, constant: 8),
+            titleLabel.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
+
+    // 프로그레스 업데이트 함수
+    private func updateProgress() {
+        let totalWidth = self.bounds.width
+        let progressWidth = totalWidth * progress
+        progressWidthConstraint.constant = progressWidth
+
+        progressIndicatorImageView.isHidden = progress >= 1.0
+        UIView.animate(withDuration: 0.25) {
+            self.progressIndicatorImageView.center.x = progressWidth
+            self.layoutIfNeeded()
+        }
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        // 프로그레스 바의 너비가 변경될 때마다 업데이트
+        updateProgress()
+    }
+}
---
+++ RhythmTokTok/Temp/Kkugy/MusicPracticeViewController.swift
@@ -0,0 +1,149 @@
+//
+//  MusicPracticeViewController.swift
+//  RhythmTokTok
+//
+//  Created by sungkug_apple_developer_ac on 10/15/24.
+//
+
+import UIKit
+
+class MusicPracticeViewController: UIViewController {
+    // 커스텀 네비게이션 바 선언
+    let practicNavBar = PracticeNavigationBar()
+    // MusicPracticeTitleView 선언
+    let musicPracticeTitleView = MusicPracticeTitleView()
+    // 테이블뷰 선언
+    let tableView = UITableView()
+    let progressData: [CGFloat] = [1.0, 1.0, 1.0, 1.0, 0.4] // TODO: 여기에 줄 진행 정도 비율 계산 로직 연결 필요
+    // Divider 역할을 할 선
+    let divider: UIView = {
+        let view = UIView()
+        view.backgroundColor = .lightGray // 선의 색상
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    let playPauseButton = PlayPauseButton(frame: CGRect(x: 0, y: 0, width: 160, height: 80))
+    let resumeButton: UIButton = {
+        let button = UIButton(type: .system)
+        let configuration = UIImage.SymbolConfiguration(pointSize: 16, weight: .regular)
+        let image = UIImage(systemName: "stop.fill", withConfiguration: configuration)
+        button.tintColor = .white
+        button.setImage(image, for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .gray08
+        button.layer.cornerRadius = 16
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.clipsToBounds = true
+        button.isHidden = false // 나중에 이값으로 활성화 관리
+        return button
+    }()
+
+    override func loadView() {
+        // 루트 뷰를 설정할 컨테이너 뷰 생성
+        let containerView = UIView()
+        containerView.backgroundColor = .white
+        // 커스텀 네비게이션 바 추가
+        containerView.addSubview(practicNavBar)
+        practicNavBar.translatesAutoresizingMaskIntoConstraints = false
+        // MusicPracticeView 추가
+        containerView.addSubview(musicPracticeTitleView)
+        musicPracticeTitleView.translatesAutoresizingMaskIntoConstraints = false
+        containerView.addSubview(divider) // divider
+
+        // 루트 뷰 설정
+        self.view = containerView
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        setupConstraints()
+    }
+    
+    private func setupUI() {
+        // 테이블 뷰 설정
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(ProgressButtonTableViewCell.self, forCellReuseIdentifier: "ProgressButtonCell")
+        view.addSubview(tableView)
+        tableView.separatorStyle = .none // 구분선 없애기
+        
+        musicPracticeTitleView.titleLabel.text = "MoonRiver" // TODO: 여기에 제목 연결
+        musicPracticeTitleView.pageLabel.text = "0/0장" // TODO: 여기에 페이지 내용 만들 함수 연결
+        
+        // 버튼을 뷰에 추가
+        playPauseButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(playPauseButton)
+        // resume 버튼 추가
+        view.addSubview(resumeButton)
+    }
+    
+    private func setupConstraints() {
+        // 커스텀 네비게이션 바와 MusicPracticeView 레이아웃 설정
+        NSLayoutConstraint.activate([
+            // 커스텀 네비게이션 바 레이아웃 설정
+            practicNavBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            practicNavBar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            practicNavBar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            practicNavBar.heightAnchor.constraint(equalToConstant: 60),
+            
+            // MusicPracticeView 레이아웃 설정 (네비게이션 바 아래에 위치)
+            musicPracticeTitleView.topAnchor.constraint(equalTo: practicNavBar.bottomAnchor, constant: 10),
+            musicPracticeTitleView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            musicPracticeTitleView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            musicPracticeTitleView.heightAnchor.constraint(equalToConstant: 30),
+
+            // divider
+            divider.topAnchor.constraint(equalTo: musicPracticeTitleView.bottomAnchor, constant: 16),
+            divider.leadingAnchor.constraint(equalTo: view.leadingAnchor), // 좌우 패딩 없이 전체 너비
+            divider.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            divider.heightAnchor.constraint(equalToConstant: 1),  // 1pt 너비로 가로선 추가
+            
+            // 테이블뷰
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            tableView.topAnchor.constraint(equalTo: divider.topAnchor, constant: 24),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            // 플레이버튼
+            playPauseButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            playPauseButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor,
+                                                    constant: -20),
+            playPauseButton.heightAnchor.constraint(equalToConstant: 80),
+            playPauseButton.widthAnchor.constraint(equalToConstant: 160),
+            
+            // 정지버튼
+            resumeButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor,
+                                                    constant: -20),
+            resumeButton.trailingAnchor.constraint(equalTo: playPauseButton.leadingAnchor, constant: -8),
+            resumeButton.heightAnchor.constraint(equalToConstant: 80),
+            resumeButton.widthAnchor.constraint(equalToConstant: 80)
+        ])
+    }
+}
+
+// MARK: - UITableViewDataSource, UITableViewDelegate
+extension MusicPracticeViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return progressData.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ProgressButtonCell", for: indexPath) as! ProgressButtonTableViewCell
+        let progress = progressData[indexPath.row]
+        cell.configure(progress: progress)
+        cell.setTitle(buttonName: "\(indexPath.row)") // TODO: 장 줄 네임 연결
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Progress button at row \(indexPath.row) clicked")
+        // TODO: 여기에 버튼 클릭 시 해당 줄부터 연주되게 만들기
+    }
+}
