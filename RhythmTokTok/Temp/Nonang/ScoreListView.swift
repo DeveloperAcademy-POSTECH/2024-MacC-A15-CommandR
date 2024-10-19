671c2226d059578d4f1a506b3564fdd647811503
norang <155873757+norang00@users.noreply.github.com>
2024-10-19 22:16:01 +0900

    네비게이션 연결하기 (#70)
    * Style: 리스트 선택 시 버튼에만 선택 효과 주기
    
    * Feature: Navigation 연결

--- RhythmTokTok/Temp/Nonang/ScoreListView.swift
+++ RhythmTokTok/Temp/Nonang/ScoreListView.swift
@@ -4,20 +4,12 @@
 //
 //  Created by Kyuhee hong on 10/15/24.
 //
+
 import UIKit
 
 class ScoreListView: UIView {
     
-    // 테이블뷰 선언
-    let tableView: UITableView = {
-        let tableView = UITableView()
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.backgroundColor = .gray03
-        
-        return tableView
-    }()
-    
-    // 제목 라벨 선언
+    // 테이블 라벨 선언
     let tableHeaderLabel: UILabel = {
         let label = UILabel()
         label.text = "악보 목록"
@@ -27,18 +19,23 @@ class ScoreListView: UIView {
         return label
     }()
     
+    // 테이블 선언
+    let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = .gray03
+        return tableView
+    }()
+    
     // 하단 버튼 선언
     let addButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle(" 악보 추가하기", for: .normal)
         button.setImage(UIImage(systemName: "document"), for: .normal)
-        
-        // 버튼 스타일 설정
         button.tintColor = .white
         button.backgroundColor = UIColor.systemBlue
         button.layer.cornerRadius = 12
         button.translatesAutoresizingMaskIntoConstraints = false
-        
         return button
     }()
     
@@ -56,28 +53,19 @@ class ScoreListView: UIView {
     private func setupView() {
         backgroundColor = UIColor.systemGray6
         
-        // 테이블뷰 설정
+        addSubview(tableHeaderLabel)
         addSubview(tableView)
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        
-        // 테이블뷰 헤더로 타이틀 설정
-        let headerView = UIView()
-        headerView.addSubview(tableHeaderLabel)
-        headerView.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 60)
-        tableView.tableHeaderView = headerView
-        
-        // 하단 버튼 추가
         addSubview(addButton)
         
         // Auto Layout 설정
         NSLayoutConstraint.activate([
-            // "악보 목록" 헤더
-            tableHeaderLabel.leadingAnchor.constraint(equalTo: headerView.leadingAnchor, constant: 16),  // 왼쪽 정렬
-            tableHeaderLabel.trailingAnchor.constraint(equalTo: headerView.trailingAnchor, constant: -8),
-            tableHeaderLabel.centerYAnchor.constraint(equalTo: headerView.centerYAnchor),  // 수직 중앙 정렬
-            
-            // 테이블 뷰 레이아웃
-            tableView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            // 헤더 레이블 레이아웃
+            tableHeaderLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 16),
+            tableHeaderLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            tableHeaderLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+
+            // 테이블뷰 레이아웃
+            tableView.topAnchor.constraint(equalTo: tableHeaderLabel.bottomAnchor, constant: 0),
             tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
             tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
             tableView.bottomAnchor.constraint(equalTo: bottomAnchor),
