4d1cadf72b2d49f8e8a71192eec2505050d86137
Kkugy <tjdrnrdla@hanmail.net>
2024-11-03 15:56:31 +0900

    Chore: 워치연결 아이콘 크기 변경

--- RhythmTokTok/Views/Components/PracticeNavigationBar.swift
+++ RhythmTokTok/Views/Components/PracticeNavigationBar.swift
@@ -86,8 +86,8 @@ class PracticeNavigationBar: UIView {
             rightButtonStackView.centerYAnchor.constraint(equalTo: centerYAnchor),
             
             // 버튼 크기 설정
-            watchConnectImageView.widthAnchor.constraint(equalToConstant: 20),
-            watchConnectImageView.heightAnchor.constraint(equalToConstant: 25),
+            watchConnectImageView.widthAnchor.constraint(equalToConstant: 24),
+            watchConnectImageView.heightAnchor.constraint(equalToConstant: 24),
             watchConnectImageView.centerYAnchor.constraint(equalTo: centerYAnchor),
 
             settingButton.widthAnchor.constraint(equalToConstant: 24),
