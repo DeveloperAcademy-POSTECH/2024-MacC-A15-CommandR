453cf35f1fc77482a2199d412fb5f424d343a40e
Hyungeol94 <ekfrhkquf12@naver.com>
2024-11-20 17:40:00 +0900

    Chore: 디자인 시스템 수정사항 반영

--- RhythmTokTok/Temp/Fonts.swift
+++ RhythmTokTok/Temp/Fonts.swift
@@ -8,13 +8,14 @@
 import Foundation
 import UIKit
 enum CustomFontStyle: String {
-    case displayBold
     case displayMedium
+    case titleBold
     case heading1Bold
+    case heading1Medium
+    case heading1Regular
     case heading2Bold
     case heading2Medium
-    case heading3Bold
-    case heading3Medium
+    case heading2Regular
     case subheadingBold
     case subheadingMedium
     case subheadingRegular
@@ -24,42 +25,51 @@ enum CustomFontStyle: String {
     case body2Bold
     case body2Medium
     case body2Regular
-    case captionBold
     case captionMedium
     case captionRegular
+    case button1Medium
+    case button2Medium
+    case watchDisplayMedium
+    case watchSubheadingMedium
+    case watchBodyMedium
 }
 
-
 let customFontDict: [CustomFontStyle: UIFont] = [
-    .displayBold: UIFont(name: "Pretendard-Bold", size: 36)!,
     .displayMedium: UIFont(name: "Pretendard-Medium", size: 36)!,
-    .heading1Bold: UIFont(name: "Pretendard-Bold", size: 32)!,
-    .heading2Bold: UIFont(name: "Pretendard-Bold", size: 24)!,
-    .heading2Medium: UIFont(name: "Pretendard-Medium", size: 24)!,
-    .heading3Bold: UIFont(name: "Pretendard-Bold", size: 21)!,
-    .heading3Medium: UIFont(name: "Pretendard-Medium", size: 21)!,
+    .titleBold: UIFont(name: "Pretendard-Bold", size: 28)!,
+    .heading1Bold: UIFont(name: "Pretendard-Bold", size: 24)!,
+    .heading1Medium: UIFont(name: "Pretendard-Medium", size: 24)!,
+    .heading1Regular: UIFont(name: "Pretendard-Regular", size: 24)!,
+    .heading2Bold: UIFont(name: "Pretendard-Bold", size: 21)!,
+    .heading2Medium: UIFont(name: "Pretendard-Medium", size: 21)!,
+    .heading2Regular: UIFont(name: "Pretendard-Regular", size: 21)!,
     .subheadingBold: UIFont(name: "Pretendard-Bold", size: 18)!,
     .subheadingMedium: UIFont(name: "Pretendard-Medium", size: 18)!,
     .subheadingRegular: UIFont(name: "Pretendard-Regular", size: 18)!,
-    .body1Bold: UIFont(name: "Pretendard-Bold", size: 16)!,
-    .body1Medium: UIFont(name: "Pretendard-Medium", size: 16)!,
-    .body1Regular: UIFont(name: "Pretendard-Regular", size: 16)!,
-    .body2Bold: UIFont(name: "Pretendard-Bold", size: 14)!,
-    .body2Medium: UIFont(name: "Pretendard-Medium", size: 14)!,
-    .body2Regular: UIFont(name: "Pretendard-Regular", size: 14)!,
-    .captionBold: UIFont(name: "Pretendard-Bold", size: 12)!,
-    .captionMedium: UIFont(name: "Pretendard-Medium", size: 12)!,
-    .captionRegular: UIFont(name: "Pretendard-Regular", size: 12)!,
+    .body1Bold: UIFont(name: "Pretendard-Bold", size: 18)!,
+    .body1Medium: UIFont(name: "Pretendard-Medium", size: 18)!,
+    .body1Regular: UIFont(name: "Pretendard-Regular", size: 18)!,
+    .body2Bold: UIFont(name: "Pretendard-Bold", size: 16)!,
+    .body2Medium: UIFont(name: "Pretendard-Medium", size: 16)!,
+    .body2Regular: UIFont(name: "Pretendard-Regular", size: 16)!,
+    .captionMedium: UIFont(name: "Pretendard-Medium", size: 14)!,
+    .captionRegular: UIFont(name: "Pretendard-Regular", size: 14)!,
+    .button1Medium: UIFont(name: "Pretendard-Medium", size: 18)!,
+    .button2Medium: UIFont(name: "Pretendard-Medium", size: 16)!,
+    .watchDisplayMedium: UIFont(name: "Pretendard-Medium", size: 64)!,
+    .watchSubheadingMedium: UIFont(name: "Pretendard-Medium", size: 16)!,
+    .watchBodyMedium: UIFont(name: "Pretendard-Medium", size: 16)!,
 ]
 
 let textStyleDict: [CustomFontStyle: UIFont.TextStyle] = [
-    .displayBold: .largeTitle,
     .displayMedium: .largeTitle,
-    .heading1Bold: .title1,
-    .heading2Bold: .title2,
-    .heading2Medium: .title2,
-    .heading3Bold: .title3,
-    .heading3Medium: .title3,
+    .titleBold: .title1,
+    .heading1Bold: .title2,
+    .heading1Medium: .title2,
+    .heading1Regular: .title2,
+    .heading2Bold: .title3,
+    .heading2Medium: .title3,
+    .heading2Regular: .title3,
     .subheadingBold: .headline,
     .subheadingMedium: .headline,
     .subheadingRegular: .headline,
@@ -69,9 +79,13 @@ let textStyleDict: [CustomFontStyle: UIFont.TextStyle] = [
     .body2Bold: .callout,
     .body2Medium: .callout,
     .body2Regular: .callout,
-    .captionBold: .caption1,
     .captionMedium: .caption1,
     .captionRegular: .caption1,
+    .button1Medium: .body,
+    .button2Medium: .subheadline,
+    .watchDisplayMedium: .largeTitle,
+    .watchSubheadingMedium: .headline,
+    .watchBodyMedium: .body,
 ]
 
 extension UIFont {
@@ -81,7 +95,7 @@ extension UIFont {
             let scaledFont = metrics.scaledFont(for: customFont)
             return scaledFont
         } else {
-            return UIFont.preferredFont(forTextStyle: textStyleDict[style, default: .body])
+            return UIFont.preferredFont(forTextStyle: .body)
         }
     }
 }
