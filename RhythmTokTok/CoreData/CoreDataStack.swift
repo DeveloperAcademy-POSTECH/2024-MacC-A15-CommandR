5c49e39431936f60e504be41f1e8dc7b9bd41d36
Kkugy <tjdrnrdla@hanmail.net>
2024-11-22 11:06:14 +0900

    Chore: 에러 핸들러 추가

--- RhythmTokTok/CoreData/CoreDataStack.swift
+++ RhythmTokTok/CoreData/CoreDataStack.swift
@@ -34,49 +34,8 @@ class CoreDataStack {
             do {
                 try context.save()
             } catch {
-                print("Failed to save context: \(error)")
+                ErrorHandler.handleError(error: "Failed to save context: \(error)")
             }
         }
     }
 }
-
-// MARK: dummy data
-//
-//func createDummyScoreEntities() {
-//    let context = CoreDataStack.shared.context
-//
-//    // Example dummy data
-//    let dummyScores = [
-//        ["id": UUID().uuidString, "title": "Score 1", "bpm": 120, "soundOption": "melodyBeat", "isHapticOn": true],
-//        ["id": UUID().uuidString, "title": "Score 2", "bpm": 90, "soundOption": "melody", "isHapticOn": false],
-//        ["id": UUID().uuidString, "title": "Score 3", "bpm": 140, "soundOption": "beat", "isHapticOn": true],
-//        ["id": UUID().uuidString, "title": "Score 4", "bpm": 100, "soundOption": "mute", "isHapticOn": false]
-//    ]
-//    
-//    for scoreData in dummyScores {
-//        let score = ScoreEntity(context: context)
-//        score.id = scoreData["id"] as? String
-//        score.title = scoreData["title"] as? String
-//        score.bpm = scoreData["bpm"] as? Int64 ?? 60
-//        score.soundOption = scoreData["soundOption"] as! String
-//        score.isHapticOn = scoreData["isHapticOn"] as? Bool ?? false
-//        score.createdAt = Date()
-//        
-//        // Add dummy notes if needed
-//        let note1 = NoteEntity(context: context)
-//        note1.id = UUID().uuidString
-//        // Add other note properties if they exist
-//        
-//        let note2 = NoteEntity(context: context)
-//        note2.id = UUID().uuidString
-//        // Add other note properties if they exist
-//        score.addToNotes(NSOrderedSet(array: [note1, note2]))
-//    }
-//    
-//    do {
-//        try context.save()
-//        print("Dummy ScoreEntity data saved successfully.")
-//    } catch {
-//        print("Failed to save dummy data: \(error)")
-//    }
-//}
