c58cf247f5ccfe94f9d7974aef74f1213caa6ee9
norang <155873757+norang00@users.noreply.github.com>
2024-12-04 09:33:40 +0900

    리스트 스크롤뷰 수정 (#325)
    * feat: 리스트 스크롤뷰 나옴
    
    * 리스트뷰 스크롤 수정, 코드 정리
    
    ---------
    
    Co-authored-by: Bell-isHere <quf417@gmail.com>

--- RhythmTokTok/Views/ScoreListView.swift
+++ RhythmTokTok/Views/ScoreListView.swift
@@ -8,14 +8,19 @@
 import UIKit
 
 class ScoreListView: UIView {
+    
+    // MARK: - UI 구성 요소
+
     let navigationBar = CommonNavigationBar()
-    let divider: UIView = {
+
+    private let divider: UIView = {
         let view = UIView()
         view.backgroundColor = .backgroundTertiary
+        view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
     
-    // 테이블 라벨 선언
+    // 테이블 헤더 라벨
     let tableHeaderLabel: UILabel = {
         let label = UILabel()
         label.text = "음악 목록"
@@ -26,7 +31,7 @@ class ScoreListView: UIView {
         return label
     }()
     
-    // 테이블 선언
+    // 테이블 뷰
     let tableView: UITableView = {
         let tableView = UITableView()
         tableView.translatesAutoresizingMaskIntoConstraints = false
@@ -48,10 +53,10 @@ class ScoreListView: UIView {
         
         // 텍스트와 아이콘 간의 여백 설정
         button.contentEdgeInsets = UIEdgeInsets(top: 8, left: 12, bottom: 8, right: 12)
-
+        
         // 버튼 크기를 텍스트와 이미지에 맞게 조정
         button.sizeToFit()
-
+        
         // 그림자 설정
         button.layer.shadowColor = UIColor.black.cgColor
         button.layer.shadowOffset = CGSize(width: 0, height: 2) // 그림자의 방향과 거리
@@ -60,7 +65,8 @@ class ScoreListView: UIView {
         return button
     }()
     
-    // 초기화
+    // MARK: - 초기화
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupNavigationBar()
@@ -71,12 +77,14 @@ class ScoreListView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func setupNavigationBar() {
+    // MARK: - 설정 메서드
+
+    private func setupNavigationBar() {
         navigationBar.configure(title: "", buttonType: .main)
+        navigationBar.translatesAutoresizingMaskIntoConstraints = false
         addSubview(navigationBar)
         addSubview(divider)
-        navigationBar.translatesAutoresizingMaskIntoConstraints = false
-        divider.translatesAutoresizingMaskIntoConstraints = false
+        
         NSLayoutConstraint.activate([
             navigationBar.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
             navigationBar.leadingAnchor.constraint(equalTo: leadingAnchor),
@@ -90,45 +98,45 @@ class ScoreListView: UIView {
         ])
     }
     
-    // 뷰 구성
     private func setupView() {
         backgroundColor = .backgroundPrimary
-        let containerView: UIView = {
-            let view = UIView()
-            view.backgroundColor = .backgroundTertiary // 배경색 설정
-            view.translatesAutoresizingMaskIntoConstraints = false
-            return view
-        }()
         
-        addSubview(containerView)
-        containerView.addSubview(tableHeaderLabel)
-        containerView.addSubview(tableView)
-        containerView.addSubview(addButton)
+        addSubview(tableView)
+        addSubview(addButton)
         
-        // Auto Layout 설정
+        // 테이블 헤더 설정
+        let headerContainer = UIView()
+        headerContainer.backgroundColor = .backgroundTertiary
+        headerContainer.translatesAutoresizingMaskIntoConstraints = false
+        headerContainer.addSubview(tableHeaderLabel)
+        
         NSLayoutConstraint.activate([
-            // 컨테이너 뷰 레이아웃
-            containerView.topAnchor.constraint(equalTo: divider.bottomAnchor),
-            containerView.leadingAnchor.constraint(equalTo: leadingAnchor),
-            containerView.trailingAnchor.constraint(equalTo: trailingAnchor),
-            containerView.bottomAnchor.constraint(equalTo: bottomAnchor),
-            
-            // 헤더 레이블 레이아웃
-            tableHeaderLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 24),
-            tableHeaderLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 16),
-            tableHeaderLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -16),
-            
+            tableHeaderLabel.topAnchor.constraint(equalTo: headerContainer.topAnchor, constant: 16),
+            tableHeaderLabel.leadingAnchor.constraint(equalTo: headerContainer.leadingAnchor, constant: 24),
+            tableHeaderLabel.trailingAnchor.constraint(equalTo: headerContainer.trailingAnchor, constant: -16),
+            tableHeaderLabel.bottomAnchor.constraint(equalTo: headerContainer.bottomAnchor, constant: 0)
+        ])
+        
+        // 헤더 크기 계산
+        headerContainer.layoutIfNeeded()
+        let headerSize = headerContainer.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
+        headerContainer.frame = CGRect(x: 0, y: 0, width: frame.width, height: headerSize.height)
+        tableView.tableHeaderView = headerContainer
+        
+        // 레이아웃 제약조건 설정
+        NSLayoutConstraint.activate([
             // 테이블뷰 레이아웃
-            tableView.topAnchor.constraint(equalTo: tableHeaderLabel.bottomAnchor),
-            tableView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
-            tableView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
-            tableView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
+            tableView.topAnchor.constraint(equalTo: divider.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor),
             
-            // 하단 버튼 레이아웃
+            // 추가 버튼 레이아웃
             addButton.widthAnchor.constraint(greaterThanOrEqualToConstant: 139),
             addButton.heightAnchor.constraint(greaterThanOrEqualToConstant: 56),
-            addButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -16),
-            addButton.bottomAnchor.constraint(equalTo: containerView.safeAreaLayoutGuide.bottomAnchor, constant: -16)
+            addButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            addButton.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -16)
         ])
     }
+    
 }
